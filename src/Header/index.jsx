ce603274b80bbc6dcbf0d33cf08c9da47776f845
jabilisunal <jabilisunal@gmail.com>
2023-11-24 19:22:32 +0400

    header completed

---
+++ src/Header/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+
+        <li>Contact</li>
+
+        </ul>
+    </div>
+  )
+}
+
+export default index
